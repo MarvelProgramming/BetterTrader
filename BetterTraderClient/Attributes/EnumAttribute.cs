44b591d1f8d454095713ca875ddb9a2ccc4ff994
Marvel Johnson <MarvelJohnson@hotmail.com>
2023-06-30 19:11:53 -0700

    Implemented Item Sorting
    - Added EnumAttribute for properly showing nested Enum types in the
      Unity inspector
    - Updated .gitignore to include packages.lock.json
    - Untracked packages.lock.json since it is generated during the build
      process and doesn't need to be version controlled
    - Fixed bug where CustomDropdown wasn't properly filtering items by
      their type
    - Fixed bug where filtering items by name was filtering by config names
      instead of what was shown in the list
    - Updated UI asset bundle so that it allows for item sorting via
      property labels just above the item list
    - Updated TraderMenu to properly reset the item filtering and sorting
      properties
    - Added BepInEx.BaseLib NuGet package to stop Unity from complaining

---
+++ BetterTraderClient/Attributes/EnumAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Menthus15Mods.Valheim.BetterTraderClient.Attributes
+{
+    /// <summary>
+    /// Source: <seealso href="https://discussions.unity.com/t/enum-drop-down-menu-in-inspector-for-nested-arrays/19915/3"/>
+    /// </summary>
+    public class EnumAttribute : PropertyAttribute
+    {
+        public Type enumType;
+
+        public EnumAttribute(Type enumType)
+        {
+            this.enumType = enumType;
+        }
+    }
+}
